bd52cd1b1d3ca4cf2dbde9bd7dec24d0aa69e525
drissnafii <drissnafi3@gmail.com>
2025-03-07 15:44:39 +0000

    feat: create migrations for favorite cities, stadiums, matches, and teams with user relationships (Pivote tables)

--- database/migrations/2025_03_07_153233_create_favorite_stadiums_table.php
+++ database/migrations/2025_03_07_153233_create_favorite_stadiums_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('favorite_cities', function (Blueprint $table) {
+        Schema::create('favorite_stadiums', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('cities')->constrained('cities')->onDelete('cascade');
+            $table->foreignId('stadium_id')->constrained('stadiums')->onDelete('cascade');
+            $table->unique(['user_id', 'stadium_id']);
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('favorite_cities');
+        Schema::dropIfExists('favorite_stadiums');
     }
 };
