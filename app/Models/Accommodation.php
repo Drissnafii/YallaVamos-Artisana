b0569addf9ba9c95dc493f10d33f629d49303d37
drissnafii <drissnafi3@gmail.com>
2025-03-08 22:51:50 +0000

    feat: enhance Accommodation model with fillable attributes and relationship to City

--- app/Models/Accommodation.php
+++ app/Models/Accommodation.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accommodation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'city_id',
+        'address',
+        'type',
+        'price_min',
+        'price_max',
+        'description',
+        'image',
+    ];
+
+    // Relationships
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
