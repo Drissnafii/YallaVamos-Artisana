58064b461046f38585525bb4c55cdbd5445d0a34
drissnafii <drissnafi3@gmail.com>
2025-03-08 22:49:31 +0000

    feat: enhance Article model with fillable attributes and relationships to User and Comment

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'author_id',
+        'publication_date',
+        'image',
+        'slug',
+        'published'
+    ];
+
+    // Relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
