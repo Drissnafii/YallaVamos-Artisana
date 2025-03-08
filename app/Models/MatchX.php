fec5577e74fe00fc3a00979f84f95b8fc5352934
drissnafii <drissnafi3@gmail.com>
2025-03-08 22:46:56 +0000

    feat: enhance MatchX model with fillable attributes and relationships to Stadium and Team

--- app/Models/MatchX.php
+++ app/Models/MatchX.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class MatchX extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'team1_id',
+        'team2_id',
+        'score_team1',
+        'score_team2',
+        'status',
+        'stadium_id',
+    ];
+
+    // Relationships
+    public function stadium()
+    {
+        return $this->belongsTo(Stadium::class);
+    }
+
+    public function team1()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function team2()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function favoriteMatches()
+    {
+        return $this->hasMany(FavoriteMatch::class);
+    }
 }
