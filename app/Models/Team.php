c21dba9f1af3c52df381c9c78c12d5853a2af59f
drissnafii <drissnafi3@gmail.com>
2025-03-08 22:42:04 +0000

    feat: enhance Team model with fillable attributes and relationships to MatchX and FavoriteTeam

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'group',
+        'flag',
+    ];
+
+    // Relationships
+    public function matches()
+    {
+        return $this->hasMany(MatchX::class);
+    }
+
+    public function favoriteTeams()
+    {
+        return $this->hasMany(FavoriteTeam::class);
+    }
 }
