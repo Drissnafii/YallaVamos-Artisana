3c43091a604f2b261b0522f1c8b58da021a0a9d4
drissnafii <drissnafi3@gmail.com>
2025-03-08 22:52:35 +0000

    feat: enhance FavoriteTeam model with fillable attributes and relationships to Team and User

--- app/Models/FavoriteTeam.php
+++ app/Models/FavoriteTeam.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class FavoriteTeam extends Model
 {
-    //
+    protected $fillable = [
+        'team_id',
+        'user_id',
+    ];
+
+    // Relationships
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
