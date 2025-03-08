105e6284272b537a86f34b42b51b8d561ddbe30b
drissnafii <drissnafi3@gmail.com>
2025-03-08 16:09:56 +0000

    feat: enhance City model with fillable attributes and relationships to Stadium, Accommodation, and FavoriteCity

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'latitude',
+        'longitude',
+    ];
+
+
+    // Relationships
+    public function stadiums()
+    {
+        return $this->hasMany(Stadium::class);
+    }
+
+    public function accommodations()
+    {
+        return $this->hasMany(Accommodation::class);
+    }
+
+    public function favoriteCities()
+    {
+        return $this->hasMany(FavoriteCity::class);
+    }
 }
